0f46ea0a94fb5cdc3135af8c55dcf56e2d554589
Gabriele <gabriele.palma.bool@gmail.com>
2023-12-13 16:48:32 +0100

    create link and get mails

--- Javascript/Jv.js
+++ Javascript/Jv.js
@@ -11,3 +11,47 @@
 
 
 //SVOLGIMENTO
+
+const { createApp } = Vue;
+
+createApp({
+
+    data() {
+
+        return {
+
+            mail: 0,
+
+            Arraymails : []
+   
+        }
+    },
+    methods: {
+        
+        
+    },
+
+    mounted() {
+
+      for(let i = 0; i < 10;i++){
+
+         this.mail = 0;
+
+         //chiamata tramite libreria axios all'Api ()
+         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+            .then((risposta) => {
+
+               console.log(risposta.data.response);
+
+               this.Arraymails[i] = risposta.data.response;
+
+               console.log(this.Arraymails);
+            });
+
+      }
+
+      this.mail = 10;
+  }
+
+}).mount('#app');
